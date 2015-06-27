28122f8477f309257603cb3fa8b56f0d8e9aa45d
Robert Gerdisch <rgerdisch@gmail.com>
2015-06-27 12:35:30 -0400

    Added functionality

--- game.js
+++ game.js
@@ -6,6 +6,20 @@ var renderer;
 // SCENE OBJECTS
 var cube;
 
+// CONTROLS
+var keys = new Array();
+var controls_view = {
+	MOVE_FORWARD: 87,
+	MOVE_BACK: 83,
+	MOVE_LEFT: 65,
+	MOVE_RIGHT: 68,
+	MOVE_UP: 32
+	/*LOOK_UP: 38,
+	LOOK_DOWN: 40,
+	LOOK_LEFT: 37,
+	LOOK_RIGHT: 39*/
+};
+
 
 function init() {
 	scene = new THREE.Scene();
@@ -18,6 +32,7 @@ function init() {
 	var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
 	cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
+	cube.rotation.x = Math.PI / 8;
 	
 	var pointLight = new THREE.PointLight(0xFFEEFF);
 	pointLight.position.x = 0;
@@ -30,7 +45,6 @@ function init() {
 
 
 function update(delta) {
-	cube.rotation.x += 1 * delta;
 	cube.rotation.y += 1 * delta;
 }
 
@@ -64,4 +78,12 @@ window.onresize = function() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.
+window.addEventListener('keydown', function(e) {
+	var code = e.which || e.keyCode;
+	keys[code] = true;
+});
+
+window.addEventListener('keyup', function(e) {
+	var code = e.which || e.keyCode;
+	keys[code] = false;
+});
